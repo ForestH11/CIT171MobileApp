84ecba4c733121d896f83089beb48f019aaf7845
Benjamin Jenson <benjamin.jenson11@yahoo.com>
2022-06-11 20:36:39 -0600

    changed app.js

--- stedi-react-native-main/App.js
+++ stedi-react-native-main/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import {useState} from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from './Login.js'
 import Counter from './Counter.js';
 import SettingsScreen from './SettingsScreen.js';
 import Home from './Home.js';
@@ -13,6 +13,9 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  if(userLoggedIn) {
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -54,8 +57,12 @@ export default function App() {
     </NavigationContainer>
   );
 
-
+} else {
+  return( 
+  <Login/>
+  );
 }
+} 
 
 
 const styles = StyleSheet.create({
